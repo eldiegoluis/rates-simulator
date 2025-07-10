1e85c3d9a121f59c8f6ab222975a1db3c96ef3b4
eldiegoluis <diegoluisandradecrespo@gmail.com>
2025-07-10 15:04:49 -0400

    feat: Add frontend assets from calculator/build and update .gitignore

---
+++ app/calculator/build/assets/js/main.js
@@ -0,0 +1,7 @@
+// enable bootstrap tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// enable bootstrap popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
